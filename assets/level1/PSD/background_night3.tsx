4445b2a29834ea1288cb063e522129792fd6a5eb
Panyoo <71757463+Panyoo@users.noreply.github.com>
2020-10-22 03:06:04 +0300

    level1changes

---
+++ assets/level1/PSD/background_night3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="background_night3" tilewidth="32" tileheight="32" tilecount="640" columns="32">
+ <image source="../background_night3.png" width="1024" height="640"/>
+</tileset>
---
+++ assets/level1/PSD/mainlevbuild_B.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="mainlevbuild_B" tilewidth="32" tileheight="32" tilecount="640" columns="32">
+ <image source="../mainlevbuild_B.png" width="1024" height="640"/>
+</tileset>
